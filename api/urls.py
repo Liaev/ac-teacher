8e024e5ee17816aa11b7fd2d18a61bafcecd1591
Nick Zaaijer <nickzaaijer@gmail.com>
2022-03-03 10:11:05 +0100

    fix routing + swagger

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from api.views import CourseApiView, GradeApiView, ProjectApiView, LoginApiView
 
 
 urlpatterns = [
-    path('oauth/token', LoginApiView.as_view(), name='oauth'),
+    path('oauth/token/', LoginApiView.as_view(), name='oauth'),
     path('courses/', CourseApiView.as_view(), name='courses'),
     path('courses/<str:course_id>/', ProjectApiView.as_view(), name='projects'),
     path('projects/<str:project_id>/', GradeApiView.as_view(), name='project_grades'),
